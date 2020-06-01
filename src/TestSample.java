c547ce043a11c7ccac1ef78572dfc5f9ecfcc3a2
mijeong <zcxz1432@naver.com>
2020-06-02 00:37:57 +0900

    delete class

--- src/TestSample.java
+++
@@ -1,5 +0,0 @@
-public interface TestSample {
-	 public int LevelUp(int level);
-	 public int LevelUp(int maxhp, int not, int not2);
-  	 public int LevelUp(int maxexp, int level);
-}
--- src/game_2.java
+++
@@ -1,8 +0,0 @@
-
-public class game_2 {
-	public static void game_2(String name) {
-		
-	}
-		
-
-}
